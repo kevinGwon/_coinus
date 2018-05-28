06d0ac3e7bb5a3d8f747bc2983d705a63b57a8c4
developerGwon <developerGwon@gmail.com>
2018-05-29 02:13:46 +0900

    Update market

--- src/assets/js/entry/coinus.js
+++ src/assets/js/entry/coinus.js
@@ -7,87 +7,98 @@ class Coinus {
     this.init();
   }
   init() {
+    this.v = 0;
+    this.total = 0;
+    this.DUR = 0.5;
+
     this.$chartCircle = $('.ct-chart--circle');
     this.$chartBar = $('.ct-chart--bar');
 
-    this.$chartCircle.length && this.chartCircle();
     this.$chartBar.length && this.chartBar();
+    this.$chartCircle.length && this.chartCircle();
   }
   chartCircle() {
-    let border = this.$chartCircle.data('border'),
-    value = this.$chartCircle.data('value'),
-    total = this.$chartCircle.data('total'),
-    unit = this.$chartCircle.data('unit'),
-    average = ((value / total) * 100).toFixed(0),
-    containerHeight = this.$chartCircle.parent().outerHeight(),
-    el = null,
+    let value = this.v,
+        total = this.total,
+        border = this.$chartCircle.data('border'),
+        unit = this.$chartCircle.data('unit'),
+        average = ((value / total) * 100).toFixed(0),
+        containerHeight = this.$chartCircle.parent().outerHeight(),
+        count = {
+          start: 0
+        },
+        el = null,
+        $chartist = null;
 
-    $chartist = null;
+    // 100% 초과할 경우
+    average < 100 ? average : average = 100;
 
-  // 100% 초과할 경우
-  average < 100 ? average : average = 100;
+    el = `
+    <div lang="en" class="ct-total">${total}<span>${unit}</span></div>
+    <div lang="en" class="ct-value">${value}<span>${unit}</span></div>
+    <div lang="en" class="ct-average"><span class="ct-average-cell">Total<b>${average}</b></span></div>
+    `;
 
-  el = `
-  <div lang="en" class="ct-total">${total}<span>${unit}</span></div>
-  <div lang="en" class="ct-value">${value}<span>${unit}</span></div>
-  <div lang="en" class="ct-average"><span class="ct-average-cell">Total<b>${average}%</b></span></div>
-  `;
+    this.$chartCircle.append(el);
 
-  this.$chartCircle.append(el);
-
-  if (IG.isMobile && this.$chartCircle.data('m-border')) {
-    border = this.$chartCircle.data('m-border');
-  }
-
-  $chartist = new Chartist.Pie('.ct-chart--circle', {
-    series: [total, total, value]
-  }, {
-    donut: true,
-    showLabel: false,
-    donutWidth: border,
-    total: total
-  });
-
-  if(!IG.$html.hasClass('ie')) {
-    $chartist.on('draw', function(data) {
-      if(data.type === 'slice') {
-        var pathLength = data.element._node.getTotalLength();
-
-        data.element.attr({
-          'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
-        });
-
-        var animationDefinition = {
-          'stroke-dashoffset': {
-            id: 'anim' + data.index,
-            dur: 700,
-            from: -pathLength + 'px',
-            to:  '0px',
-            easing: Chartist.Svg.Easing.easeOutQuint,
-            fill: 'freeze'
-          }
-        };
-
-        if(data.index !== 0) {
-          animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
-        }
-
-        data.element.attr({
-          'stroke-dashoffset': -pathLength + 'px'
-        });
-
-        data.element.animate(animationDefinition, false);
-      }
+    TweenMax.to(count, this.DUR, {
+      start: average,
+      onUpdate: function () {
+        addNum($('.ct-average b'), count.start);
+      }      
     });
-  }
+
+    function addNum($target, num) {
+        num = Math.ceil(num);
+        $target.html(Math.ceil(num)+'%');
+    }
+
+    if (IG.isMobile && this.$chartCircle.data('m-border')) {
+      border = this.$chartCircle.data('m-border');
+    }
+
+    $chartist = new Chartist.Pie('.ct-chart--circle', {
+      series: [total, total, value]
+    }, {
+      donut: true,
+      showLabel: false,
+      donutWidth: border,
+      total: total
+    });
+
+    if(!IG.$html.hasClass('ie')) {
+      setTimeout(()=>{
+        let self = this,
+            $el = $('.ct-chart-donut'),
+            $path = $el.find('path');
+
+        $path.each(function(i, el){
+          let length = $(el).get(0).getTotalLength();
+
+          TweenMax.set($(el), {
+            strokeDasharray: length
+          });
+
+          TweenMax.fromTo($(el), self.DUR, {
+            delay: 0.1,
+            strokeDashoffset: length
+          }, {
+            strokeDashoffset: length * 2
+          });            
+        });
+      }, 1);
+    }
 }
 chartBar() {
   this.$chartBar.each((i, el)=>{
     let $el = $(el),
-    value = $el.data('value'),
-    total = $el.data('total'),
-    unit = $el.data('unit'),
-    average = ((value/total)*100).toFixed(1);
+        value = $el.data('value'),
+        total = $el.data('total'),
+        unit = $el.data('unit'),
+        average = ((value/total)*100).toFixed(1);
+
+        this.total += total;
+        this.v += value;
 
     $el.append('<div lang="en" class="ct-value">'+value+unit+'<span lang="en" class="ct-average">'+average+'%</span></div>');
 
