cceac39426c5f84ee1349e389b25bbb365b8846e
developerGwon <developerGwon@gmail.com>
2018-05-29 23:59:21 +0900

    Update mobile

--- src/assets/js/entry/coinus.js
+++ src/assets/js/entry/coinus.js
@@ -17,10 +17,33 @@ class Coinus {
     this.$chartBar.length && this.chartBar();
     this.$chartCircle.length && this.chartCircle();
   }
+  chartBar() {
+    this.$chartBar.each((i, el)=>{
+      let $el = $(el),
+          value = $el.data('value'),
+          total = $el.data('total'),
+          unit = $el.data('unit'),
+          average = ((value/total)*100).toFixed(1);
+
+          this.total += total;
+          this.v += value;
+
+      $el.append('<div lang="en" class="ct-value">'+value+unit+'<span lang="en" class="ct-average">'+average+'%</span></div>');
+
+      let $value = $el.find('.ct-value');
+
+      $value.css('width', average+'%');
+
+      // color invert
+      if(!IG.isMobile) {
+        $value.outerWidth() < 10 ? $value.addClass('is-invert') : '';
+      }
+    });
+  }
   chartCircle() {
-    let value = this.v,
+    let self = this,
+        value = this.v,
         total = this.total,
-        border = this.$chartCircle.data('border'),
         unit = this.$chartCircle.data('unit'),
         average = ((value / total) * 100).toFixed(0),
         containerHeight = this.$chartCircle.parent().outerHeight(),
@@ -44,25 +67,15 @@ class Coinus {
     TweenMax.to(count, this.DUR, {
       start: average,
       onUpdate: function () {
-        addNum($('.ct-average b'), count.start);
+        self.addNum($('.ct-average b'), count.start);
       }      
     });
-
-    function addNum($target, num) {
-        num = Math.ceil(num);
-        $target.html(Math.ceil(num)+'%');
-    }
-
-    if (IG.isMobile && this.$chartCircle.data('m-border')) {
-      border = this.$chartCircle.data('m-border');
-    }
 
     $chartist = new Chartist.Pie('.ct-chart--circle', {
       series: [total, total, value]
     }, {
       donut: true,
       showLabel: false,
-      donutWidth: border,
       total: total
     });
 
@@ -87,31 +100,12 @@ class Coinus {
           });            
         });
       }, 1);
-    }
-}
-chartBar() {
-  this.$chartBar.each((i, el)=>{
-    let $el = $(el),
-        value = $el.data('value'),
-        total = $el.data('total'),
-        unit = $el.data('unit'),
-        average = ((value/total)*100).toFixed(1);
-
-        this.total += total;
-        this.v += value;
-
-    $el.append('<div lang="en" class="ct-value">'+value+unit+'<span lang="en" class="ct-average">'+average+'%</span></div>');
-
-    let $value = $el.find('.ct-value');
-
-    $value.css('width', average+'%');
-
-    // color invert
-    if(!IG.isMobile) {
-      $value.outerWidth() < 10 ? $value.addClass('is-invert') : '';
-    }
-  });
-}  
+    }   
+  }  
+  addNum($target, num) {
+    num = Math.ceil(num);
+    $target.html(Math.ceil(num)+'%');
+  }   
 }
 
 $(function() {
